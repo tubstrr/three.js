1f1cac7daf7d93ae9d4d42995e7316ce72dce9e4
tubstrr <me@jonknoll.dev>
2022-10-10 07:09:22 -0500

    lesson 4

---
+++ src/js/three.js
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.getElementById("three-js");
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Resize
+window.addEventListener("resize", () => {
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.render(scene, camera);
+});
